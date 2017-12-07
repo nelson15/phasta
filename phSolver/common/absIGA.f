54dbbb5b2e9fcfaada5c956c8283c58c1f80af2a
nelson15 <corey.nelson@colorado.edu>
2017-12-07 10:57:34 -0700

    fix do loop syntax in absIGA

--- phSolver/common/absIGA.f
+++ phSolver/common/absIGA.f
@@ -6,16 +6,16 @@
      &          shpIGA(nshl,ngauss)
 
       integer e((ipord+1)^3), n(ngauss^3)
-      do i= 1:num_elem_1D
-        do j= 1:num_elem_1D
-          do k=1:num_elem_1D
-            do ex=1:ipord+1
-              do ey=1:ipord+1
-                do ez=1:ipord+1
+      do i= 1, num_elem_1D
+        do j= 1, num_elem_1D
+          do k=1, num_elem_1D
+            do ex=1, ipord+1
+              do ey=1, ipord+1
+                do ez=1, ipord+1
                   e=ex+(ey-1)*(ipord+1)+(ez-1)*(ipord+1)(ipord+1)
-                  do intpx=1:ngauss1D
-                    do intpy=1:ngauss1D
-                      do intpz=1:ngauss1D
+                  do intpx=1, ngauss1D
+                    do intpy=1, ngauss1D
+                      do intpz=1, ngauss1D
                         n=intpx+(intpy-1)*(ngauss1D)+(intpz-1)*(ngauss1D)(ngauss1D)
                         shpIGA(e,n)=Bez(i,ex,intpx)*Bez(j,ey,intpy)*
      &                              Bez(k,ez,intpz)
@@ -39,16 +39,16 @@
      &          Bezg(num_elem_1D, ipord+1,nguass1D)
      &          shglIGA(nsd,nshl,ngauss)
       integer e((ipord+1)^3), n(ngauss^3)
-      do i= 1:num_elem_1D
-        do j= 1:num_elem_1D
-          do k=1:num_elem_1D
-            do ex=1:ipord+1
-             do ey=1:ipord+1
-               do ez=1:ipord+1
+      do i= 1, num_elem_1D
+        do j= 1, num_elem_1D
+          do k=1, num_elem_1D
+            do ex=1, ipord+1
+             do ey=1, ipord+1
+               do ez=1, ipord+1
                  e=ex+(ey-1)*(ipord+1)+(ez-1)*(ipord+1)(ipord+1)
-                 do intpx=1:ngauss1D
-                   do intpy=1:ngauss1D
-                     do intpz=1:ngauss1D
+                 do intpx=1, ngauss1D
+                   do intpy=1, ngauss1D
+                     do intpz=1, ngauss1D
                        n=intpx+(intpy-1)*(ngauss1D)+(intpz-1)*(ngauss1D)(ngauss1D)
                        shglIGA(1,e,n)=Bezg(i,ex,intpx)*Bez(j,ey,intpy)*
      &                                Bez(k,ez,intpz)
